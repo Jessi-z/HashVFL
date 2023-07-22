ab46100c0cb2ce9c8d8a0a84e50f166ba0a67895
Jessi-z <74857224+Jessi-z@users.noreply.github.com>
2023-07-22 08:38:02 +0800

    Update function.py

--- utils/function.py
+++ utils/function.py
@@ -106,20 +106,6 @@ def loadModels(root, dataset_name, num_features, encode_length, defense, epsilon
             path = os.path.join(root, 'worker_{}.pt'.format(i))
             worker.load_state_dict(torch.load(path))
             workers.append(worker)
-    elif dataset_name in ['imdb']:
-        for i, num_feature in enumerate(num_features):
-            worker = BertBaseModel(encode_length, defense, epsilon, device).to(device)
-            path = os.path.join(root, 'worker_{}.pt'.format(i))
-            worker.load_state_dict(torch.load(path))
-            workers.append(worker)
-    elif dataset_name in ['mnist', 'cifar10', 'cifar100', 'emotion']:
-        in_channels = 1 if dataset_name in ['mnist'] else 3
-        for i, num_feature in enumerate(num_features):
-            worker = MyResNet(in_channels=in_channels, encode_length=encode_length, defense=defense, epsilon=epsilon,
-                              device=device).to(device)
-            path = os.path.join(root, 'worker_{}.pt'.format(i))
-            worker.load_state_dict(torch.load(path))
-            workers.append(worker)
     else:
         logging.info('Not supported datatype!')
         exit()
@@ -137,15 +123,6 @@ def loadBottomModels(dataset_name, num_features, encode_length, epsilon, defense
             workers.append(
                 MLP(in_features=num_feature, out_features=encode_length, defense=defense, epsilon=epsilon,
                     device=device, num_layers=num_layers).to(device))
-    elif dataset_name in ['imdb']:
-        for _ in range(len(num_features)):
-            workers.append(BertBaseModel(encode_length, defense, epsilon, device).to(device))
-    elif dataset_name in ['mnist', 'cifar10', 'cifar100', 'emotion']:
-        in_channels = 1 if dataset_name in ['mnist'] else 3
-        for _ in range(len(num_features)):
-            workers.append(
-                MyResNet(in_channels=in_channels, encode_length=encode_length, defense=defense, epsilon=epsilon,
-                         device=device).to(device))
     else:
         logging.info('Not supported datatype!')
         exit()
